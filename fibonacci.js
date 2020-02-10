b5a6fc17f455eff9d51e1695f7b30feeb467d162
Alunos Laboratorio <aluno.laboratorio@faesa.br>
2020-02-10 20:10:24 -0300

    Primeiro commit

---
+++ fibonacci.js
@@ -0,0 +1,12 @@
+var n1 = 0;
+var n2 = 1;
+var resultado = n1 + n2;
+
+console.log(n1);
+
+for (var i=0;i<=17;i++){
+  console.log(resultado);
+  n1 = n2;
+  n2 = resultado;
+  resultado = n1 + n2;
+}
