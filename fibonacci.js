47d0344d4900b3e2f82d4776074e53242428aba6
guilherme-santos-machado <machado.guilherme@live.com>
2020-02-11 14:09:41 +0000

    add n2 log

--- fibonacci.js
+++ fibonacci.js
@@ -3,10 +3,12 @@ var n2 = 1;
 var resultado = n1 + n2;
 
 console.log(n1);
+console.log(n2);
 
 for (var i=0;i<=17;i++){
-  console.log(resultado);
-  n1 = n2;
-  n2 = resultado;
-  resultado = n1 + n2;
+console.log(resultado);
+n1 = n2;
+n2 = resultado;
+resultado = n1 + n2;
 }
+
